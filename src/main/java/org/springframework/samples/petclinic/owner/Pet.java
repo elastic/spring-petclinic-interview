d08fe0501a5d0fa61b3a69dcb71f61746b15a26f
Kathleen DeRusso <kathleen.derusso@elastic.co>
2023-07-28 10:42:47 -0400

    Initial commit

--- src/main/java/org/springframework/samples/petclinic/owner/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Pet.java
@@ -18,6 +18,7 @@ package org.springframework.samples.petclinic.owner;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -52,6 +53,12 @@ public class Pet extends NamedEntity {
 	@JoinColumn(name = "type_id")
 	private PetType type;
 
+	@JoinColumn(name = "sex")
+	private Character sex;
+
+	@JoinColumn(name = "color")
+	private String color;
+
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	@JoinColumn(name = "pet_id")
 	@OrderBy("visit_date ASC")
@@ -73,6 +80,26 @@ public class Pet extends NamedEntity {
 		this.type = type;
 	}
 
+	public Character getSex() {
+		return this.sex;
+	}
+
+	public void setSex(Character sex) throws Exception {
+		if (sex == null) {
+			throw new Exception("sex not specified");
+		}
+		this.sex = sex;
+	}
+
+	public String getColor() {
+		return this.color;
+	}
+
+	public void setColor(String color) {
+		System.out.println("old color:" + this.color + "; new color: " + color);
+		this.color = color;
+	}
+
 	public Collection<Visit> getVisits() {
 		return this.visits;
 	}
@@ -81,4 +108,13 @@ public class Pet extends NamedEntity {
 		getVisits().add(visit);
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Pet pet = (Pet) o;
+		return Objects.equals(birthDate, pet.birthDate) && Objects.equals(type, pet.type) && Objects.equals(sex, pet.sex) && Objects.equals(color,
+			pet.color
+		) && Objects.equals(visits, pet.visits);
+	}
 }
